0a54e16f39ce9a79ec726668398e9db2e494af37
nguyen199801 <nguyennc710@gmail.com>
2020-08-31 04:22:53 +1000

    fixed filter function, output in console

--- textAttribute.js
+++ textAttribute.js
@@ -1,9 +1,11 @@
-$.getJSON("database.json", function(data){
-    console.log('data', data);
-});
 function FilterArray(item,value){
 	const result = item.filter(it=>it.name==value||it.code==value||it.color==value)
 	return result;
 }
-	const result = FilterArray(data,"BLUE");
+
+$.getJSON("database.json", function(data){
+    const result = FilterArray(data,"BLUE");
 	console.log(result);
+});
+
+	
